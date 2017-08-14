ec02da2ad9830977ea8c566ee94fc0acb5d5689f
Gustavo Ericson <gustavo.ericson@gmail.com>
2017-08-14 14:39:18 -0300

    Added search and login (currently empty)

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,10 +1,18 @@
 <header role="document-header">
+
+    {{--Items à esquerda na barra--}}
     <nav class="level-left">
         @include('layouts.partials.header.brand')
         @include('layouts.partials.header.toolmenu')
-
     </nav>
-    <nav class="level-item"></nav>
-    <nav class="level-right"></nav>
 
+    {{--Items no centro--}}
+    <nav class="level-item">
+    </nav>
+
+    {{--Items à direita--}}
+    <nav class="level-right">
+        @include('layouts.partials.header.search')
+        @include('layouts.partials.header.login')
+    </nav>
 </header>
