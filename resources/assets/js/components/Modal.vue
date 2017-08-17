3440194618e914eeeee0c4da075b652e041f453c
Gustavo Ericson <gustavo.ericson@gmail.com>
2017-08-17 11:52:12 -0300

    Implement Animate.css

--- resources/assets/js/components/Modal.vue
+++ resources/assets/js/components/Modal.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="modal is-active">
-        <div class="modal-background" @click="$emit('close')"></div>
-        <div class="modal-content">
-            <div class="modal-card">
-                <div class="modal-card-head">
-                    <h1 class="is-size-4"><strong>{{ title }}</strong></h1>
-                </div>
-                <div class="modal-card-body">
-                    <slot></slot>
-                </div>
-                <!--<div class="modal-card-foot">-->
+        <div class="modal is-active">
+            <div class="modal-background animated fadeIn" @click="$emit('close')"></div>
+            <div class="modal-content animated fadeInUp">
+                <div class="modal-card">
+                    <div class="modal-card-head">
+                        <h1 class="is-size-4"><strong>{{ title }}</strong></h1>
+                    </div>
+                    <div class="modal-card-body">
+                        <slot></slot>
+                    </div>
+                    <!--<div class="modal-card-foot">-->
                     <!--<button class="button is-primary">Enviar</button>-->
-                <!--</div> TODO Colocar modal footer como opcional, se der -->
+                    <!--</div> TODO Colocar modal footer como opcional, se der -->
+                </div>
             </div>
+            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
         </div>
-        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
-    </div>
 </template>
 
 <script>
