f42e89856b4836745a3d91406980952aa77c594f
Gustavo Ericson <gustavo.ericson@gmail.com>
2017-08-21 15:53:46 -0300

    Login will hide login and register on auth sessions. HomeController will redirect to same page on login.

--- resources/views/layouts/partials/header/login.blade.php
+++ resources/views/layouts/partials/header/login.blade.php
@@ -1,10 +1,22 @@
 @include('layouts.partials.header.login.modalLogin')
 @include('layouts.partials.header.login.modalRegister')
 
-<div class="level-item header-item">
-    <a href="#" @click="showLoginModal = true">Login</a>
-</div>
+@if(Auth::check())
+    <div id="login-badge" class="level-item login-badge">
+        <div class="user-image">
+            <img src="http://via.placeholder.com/40x40">
+        </div>
+        <div class="user-data">
+            <span id="user-name">{{ Auth::user()->nome_curto }}</span>
+            <span id="user-unidade">{{ Auth::user()->unidade->sigla }} &nbsp <a href="/sair">(sair)</a></span>
+        </div>
+    </div>
+@else
+    <div class="level-item header-item">
+        <a href="#" @click="showLoginModal = true">Login</a>
+    </div>
 
-<div class="level-item header-item">
-    <a href="#" @click="showRegistrationModal = true">Cadastre-se</a>
-</div>
+    <div class="level-item header-item">
+        <a href="#" @click="showRegistrationModal = true">Cadastre-se</a>
+    </div>
+@endif
