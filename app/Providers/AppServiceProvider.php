f638f28068fdd4536421d7b8053f1007e12d4363
Gustavo Ericson <gustavo.ericson@gmail.com>
2017-08-31 14:40:31 -0300

    Added locale and default format for Carbon instances

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 use Laracasts\Generators\GeneratorsServiceProvider;
 
@@ -14,6 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Carbon::setLocale(config('app.locale'));
+        Carbon::setToStringFormat('d/m/Y');
         //
     }
 
