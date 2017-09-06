ae7d5428909ce5d3631b43c9f7c3333c110a8d80
Gustavo Ericson <gustavo.ericson@gmail.com>
2017-09-06 11:50:36 -0300

    Implemented Vinculos

--- database/migrations/2017_09_05_193650_create_vinculos_table.php
+++ database/migrations/2017_09_05_193650_create_vinculos_table.php
@@ -19,36 +19,35 @@ class CreateVinculosTable extends Migration
             $table->string('descricao');
         });
 
-//        Schema::create('vinculos', function (Blueprint $table) {
-//            $table->increments('id');
-//            $table->timestamps();
+        Schema::create('vinculos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->integer('user_rh_id')->unsigned();
+            $table->integer('supervisor_id')->unsigned()->nullable();
+
+            $table->string('orgao_origem')->nullable();
+            $table->string('matricula_origem')->nullable();
+            $table->string('cargo_origem')->nullable();
+            $table->string('classe')->nullable();
+            $table->string('padrao')->nullable();
+            $table->string('funcao')->nullable();
+            $table->string('denominacao_funcao')->nullable();
+            $table->string('ato_nomeacao')->nullable();
+            $table->date('data_dou')->nullable();
+            $table->string('empresa')->nullable();
+            $table->string('instituicao_ensino')->nullable();
+            $table->string('nivel')->nullable();
+            $table->string('curso')->nullable();
+            $table->string('semestre')->nullable();
+            $table->string('numero_contrato')->nullable();
+            $table->date('data_contrato')->nullable();
+        });
 //
-//            $table->integer('user_rh_id')->unsigned();
-//
-//            $table->string('orgao_origem')->nullable();
-//            $table->string('matricula_origem')->nullable();
-//            $table->string('cargo_origem')->nullable();
-//            $table->string('classe')->nullable();
-//            $table->string('padrao')->nullable();
-//            $table->string('funcao')->nullable();
-//            $table->string('denominacao_funcao')->nullable();
-//            $table->string('ato_nomeacao')->nullable();
-//            $table->date('data_dou')->nullable();
-//            $table->string('empresa')->nullable();
-//            $table->string('instituicao_ensino')->nullable();
-//            $table->string('nivel')->nullable();
-//            $table->string('curso')->nullable();
-//            $table->string('semestre')->nullable();
-//            $table->string('numero_contrato')->nullable();
-//            $table->date('data_contrato')->nullable();
-//            $table->string('orgao_origem')->nullable();
-//            $table->string('supervisor_id')->unsigned()->nullable();
-//        });
-//
-//        Schema::table('vinculos', function (Blueprint $table) {
-//            $table->foreign('user_rh_id')->references('user_id')->on('users_rh')->onDelete('cascade');
-//            $table->foreign('supervisor_id')->references('user_id')->on('users_rh')->onDelete('set null');
-//        });
+        Schema::table('vinculos', function (Blueprint $table) {
+            $table->foreign('user_rh_id')->references('user_id')->on('users_rh')->onDelete('cascade');
+            $table->foreign('supervisor_id')->references('user_id')->on('users_rh')->onDelete('set null');
+        });
 
     }
 
